851c0bf66e78193029729d113611bd7598124766
thelazyone <giacomo.pantalone@gmail.com>
2024-01-20 16:20:03 +0100

    added basic parts of the YEW app.

---
+++ fsd28-app/static/js/download_file.js
@@ -0,0 +1,21 @@
+function downloadFile(content, filename) {
+    const blob = new Blob([content], { type: 'application/json' });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+    a.click();
+    window.URL.revokeObjectURL(url);
+}
+
+function downloadFileWithPath(content, defaultName) {
+    const blob = new Blob([content], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.style.display = 'none';
+    a.href = url;
+    a.download = defaultName;
+    document.body.appendChild(a);
+    a.click();
+    URL.revokeObjectURL(url);
+}
